07b872395c109a70df179bdf23c9af7a0a996178
Xristoskrik <krikxristos@gmail.com>
2024-11-09 14:46:41 +0200

    added frontend_signup_page

--- frontend/yourmusic/src/routes/+page.svelte
+++ frontend/yourmusic/src/routes/+page.svelte
@@ -1,6 +1,77 @@
 <script>
-
 </script>
+
 <main>
-    <p>index</p>
+    <div class="signup">
+        <h1 class="logo">yourMusic 🎶</h1>
+        <p class="logo">Sign Up</p>
+        <form action="">
+            <label for="username">Username:</label>
+            <input type="username" id="username" />
+            <label for="email">Email:</label>
+            <input type="email" id="email" />
+            <label for="password">Password:</label>
+            <input type="password" id="password" />
+            <button>Sign Up!</button>
+        </form>
+    </div>
 </main>
+
+<style>
+    main {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: #121212;
+    }
+    .signup {
+        width: 40%;
+        height: 70%;
+        transition: width 0.5s ease-in-out;
+        color: white;
+        background-color: #1c1c1e;
+        border: 8px solid #0d0d0d;
+        border-radius: 3vh;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    p {
+        font-size: 1.5rem;
+    }
+    h1 {
+        margin-top: 2rem;
+        font-size: 4vh;
+        font-family: roboto;
+        letter-spacing: 0.3vh;
+    }
+
+    form {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    label {
+        margin-bottom: 1vh;
+    }
+    button {
+        margin-top: 4vh;
+        padding: 1vh;
+        width: 100%;
+        background-color: green;
+        border: 5px solid green;
+        border-radius: 3vh;
+    }
+    input {
+        width: 100%;
+        padding: 1vh;
+        margin-bottom: 1vh;
+    }
+    @media (max-width: 600px) {
+        .signup {
+            width: 70%;
+        }
+    }
+</style>
--- frontend/yourmusic/src/routes/tracks/+page.svelte
+++ frontend/yourmusic/src/routes/tracks/+page.svelte
@@ -1,80 +1,96 @@
 <script>
     let audio;
     const playAudio = () => {
-       if(audio.paused){
-        audio.play();
-       } else{
-        audio.pause();
-       }
-       
-    }
-    const songs = ["ena","duo","tria","tria","tria","tria","tria","tria","tria","tria"]
+        if (audio.paused) {
+            audio.play();
+        } else {
+            audio.pause();
+        }
+    };
+    const songs = [
+        "ena",
+        "duo",
+        "tria",
+        "tria",
+        "tria",
+        "tria",
+        "tria",
+        "tria",
+        "tria",
+        "tria",
+    ];
 </script>
-<main>
-    <div class = "songs">
-    {#each songs as song }
-     <div class = "song">
-        <img class = "profile-details" src="./image.png" alt="image of artist">
-        <div class = "song-details" >
-        <p>artist name</p>
-        <p>{song}     00:00</p>
-        
-        </div>
-        <div class = "song-details" >
-        
-            <button class="play-song" on:click={playAudio}>►</button>
-            
-            </div>
 
-      </div>
-    {/each}
+<main>
+    <div class="songs">
+        {#each songs as song}
+            <div class="song">
+                <img
+                    class="profile-details"
+                    src="./image.png"
+                    alt="image of artist"
+                />
+                <div class="song-details">
+                    <p>artist name</p>
+                    <p>{song} 00:00</p>
+                </div>
+                <div class="song-details">
+                    <button class="play-song" on:click={playAudio}>►</button>
+                </div>
+            </div>
+        {/each}
     </div>
-<div class="audio-player">
-    <div class = "audio-player-details"> 
-        <img class = "profile" src="./image.png" alt="image of artist">
-        <div class  >
-        <p>artist name</p>
-        <p>song name</p>
+    <div class="audio-player">
+        <div class="audio-player-details">
+            <img class="profile" src="./image.png" alt="image of artist" />
+            <div class>
+                <p>artist name</p>
+                <p>song name</p>
+            </div>
+        </div>
+        <div class="audio-player-center">
+            <audio bind:this={audio} id="audio" src="./music.mp3"></audio>
+            <div class="button-wrapper">
+                <button class="audiobutton" on:click={playAudio}>►</button>
+            </div>
+            <div class="slider-wrapper">
+                <div class="time-display">0:00</div>
+                <input
+                    type="range"
+                    id="seek-bar"
+                    value="0"
+                    max="100"
+                    step="1"
+                />
+                <div class="time-display">0:00</div>
+            </div>
         </div>
     </div>
-    <div class = "audio-player-center">
-    <audio bind:this={audio}  id="audio" src="./music.mp3"></audio>
-    <div class="button-wrapper">
-        <button class="audiobutton" on:click={playAudio}>►</button>
-    </div>
-    <div class="slider-wrapper">
-        <div class="time-display">0:00</div>
-        <input type="range" id="seek-bar" value="0" max="100" step="1">
-        <div class="time-display">0:00</div>
-    </div>
-   
-   
-    </div>
-</div>
 </main>
+
 <style>
-    main{
+    main {
         display: flex;
         flex-direction: column;
         align-items: flex-end;
-        width:  100%;
+        width: 100%;
         height: 100%;
         background-color: rgb(0, 0, 0);
     }
-    .profile-details{
+    .profile-details {
         width: auto;
         height: 100%;
         margin-left: 25%;
         color: white;
     }
 
-    .song-details{
+    .song-details {
         width: 20%;
         font-size: 3vh;
         color: white;
         letter-spacing: 0.2vh;
     }
-    .play-song{
+    .play-song {
         font-size: 4vh;
     }
     .songs {
@@ -82,15 +98,15 @@
         flex-direction: column;
         justify-content: flex-start;
         align-items: center;
-        flex-wrap: nowrap;   
+        flex-wrap: nowrap;
         overflow-x: auto;
         align-items: center;
-   
+
         width: 100%;
         height: 100%;
         background-color: rgb(12, 43, 71);
     }
-    .song{
+    .song {
         width: 50%;
         height: 20%;
         display: flex;
@@ -98,42 +114,39 @@
         border-radius: 5vh;
         flex-direction: row;
         border: 1vh solid rgb(20, 12, 2);
-        border-radius: 2vh;    
+        border-radius: 2vh;
         margin: 1vh;
         padding: 1vh;
         background-color: rgb(20, 12, 2);
-      
     }
-    .profile{
+    .profile {
         margin-top: 1vh;
+        margin-left: 1vh;
         max-width: 5em;
         max-height: 5em;
     }
-    .audio-player-details{
+    .audio-player-details {
         display: flex;
         flex-direction: row;
         background-color: aqua;
         width: 25%;
         height: 100%;
     }
-    .audio-player{
+    .audio-player {
         display: flex;
         align-items: flex-end;
         justify-content: flex-start;
-        width:  100%;
-        height: 10%;
+        width: 100%;
+        height: 20%;
         background-color: rgb(36, 12, 247);
-     
-      
-     
     }
-    .button-wrapper{
+    .button-wrapper {
         margin-top: 3vh;
     }
     .slider-wrapper {
         display: flex;
         align-items: center;
-        gap: 10px; 
+        gap: 10px;
         width: 100%;
         justify-content: center;
         margin-top: 1vh;
@@ -143,7 +156,7 @@
         color: white;
         font-size: 14px;
     }
-    .audio-player-center{
+    .audio-player-center {
         display: flex;
         flex-direction: column;
         align-items: center;
@@ -151,85 +164,69 @@
         width: 55%;
         height: 100%;
         background-color: darkgreen;
-     
     }
 
-    input[type="range"]{
+    input[type="range"] {
         width: 60%;
-       
-    
-
     }
-    @media(max-width :1135px){
-        .song-details{
-        width: 10%;
-        font-size: 2.5vh;
-        color: white;
-        letter-spacing: 0vh;
+    @media (max-width: 1135px) {
+        .song-details {
+            width: 10%;
+            font-size: 2.5vh;
+            color: white;
+            letter-spacing: 0vh;
         }
-        .profile-details{
-      
-        margin-left: 1vh;
+        .profile-details {
+            margin-left: 1vh;
+        }
+        .play-song {
+            font-size: 3vh;
+            margin-left: 3.5vh;
+        }
     }
-        .play-song{
-        font-size: 3vh;
-        margin-left: 3.5vh;
-    }
-    
-    }
-    @media(max-width :805px){
-        .audio-player-details{
-        width:  100%;
-        height: 50%;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-    }
-    .audio-player{
-  
-       
-        width:  100%;
-        height: 50%;
-        display: flex;
-        align-items: flex-start;
-        justify-content: center;
-        flex-direction: column;
-        background-color: rgb(25, 0, 250);
-       
-    }
+    @media (max-width: 805px) {
+        .audio-player-details {
+            width: 100%;
+            height: 50%;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+        .audio-player {
+            width: 100%;
+            height: 50%;
+            display: flex;
+            align-items: flex-start;
+            justify-content: center;
+            flex-direction: column;
+            background-color: rgb(25, 0, 250);
+        }
 
-    .slider-wrapper {
-        width:  100%;
-        height: 100%;
-        display: flex;
-        align-items: flex-end;
-        justify-content: center;
-    }
-    .audio-player-center{
-     
-        width:  100%;
-        height: 70%;
-        display: flex;
-        align-items:center;
-        justify-content: center;
-       
-      
-        background-color: rgb(7, 100, 7);
-     
-    }
-    .time-display {
-       margin-bottom: 5vh;
-    }
-    .button-wrapper{
-        margin-top: 7vh;
-    }
-    input[type="range"]{
-        width: 60%;
-        margin-bottom: 5vh;
+        .slider-wrapper {
+            width: 100%;
+            height: 100%;
+            display: flex;
+            align-items: flex-end;
+            justify-content: center;
+        }
+        .audio-player-center {
+            width: 100%;
+            height: 70%;
+            display: flex;
+            align-items: center;
+            justify-content: center;
 
-       
-    
-
-    }
+            background-color: rgb(7, 100, 7);
+        }
+        .time-display {
+            margin-bottom: 5vh;
+        }
+        .button-wrapper {
+            margin-top: 7vh;
+        }
+        input[type="range"] {
+            width: 60%;
+            margin-bottom: 5vh;
+        }
     }
 </style>
